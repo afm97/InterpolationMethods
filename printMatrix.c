b5fe439d0291ecba01a15839b5134d7948a96015
afm97 <arthurfranca97@gmail.com>
2023-09-18 10:58:47 -0300

    Correção do segundo for onde havia rows - substituido por cols

--- printMatrix.c
+++ printMatrix.c
@@ -1,7 +1,7 @@
 void printMatrix(int rows, int cols, float matrix[rows][cols]){
     for (int i = 0; i < rows; i++)
     {
-        for(int j = 0; j < rows; j++)
+        for(int j = 0; j < cols; j++)
         {
             printf("%f     ", matrix[i][j]);
         }
